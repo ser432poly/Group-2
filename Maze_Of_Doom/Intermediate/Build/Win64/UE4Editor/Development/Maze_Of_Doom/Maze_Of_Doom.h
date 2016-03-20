f9a6a98a8d9e6f323a23cd9c5325909b70bcb436
drtracy1 <drtracy1@asu.edu>
2016-03-20 14:19:35 -0700

    Setup fixed top-down camera (Task#3419)

--- Maze_Of_Doom/Intermediate/Build/Win64/UE4Editor/Development/Maze_Of_Doom/Maze_Of_Doom.h
+++ Maze_Of_Doom/Intermediate/Build/Win64/UE4Editor/Development/Maze_Of_Doom/Maze_Of_Doom.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_MAZE_OF_DOOM_H__
 #define __AUTO_MAZE_OF_DOOM_H__
-//Last Write: 3/19/2016 9:17:36 AM +00:00
+//Last Write: 3/20/2016 7:46:26 PM +00:00
 #include "C:\Users\daniel\Documents\Unreal Projects\Group2\Maze_Of_Doom\Source\Maze_Of_Doom\Maze_Of_Doom.h"
 #endif//__AUTO_MAZE_OF_DOOM_H__
