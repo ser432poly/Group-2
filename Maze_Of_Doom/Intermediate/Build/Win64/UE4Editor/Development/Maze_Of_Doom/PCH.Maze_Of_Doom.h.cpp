b67f090f409b8bdafa988943d3f39bf681f34b97
David Mierke <dmierke@asu.edu>
2016-04-28 14:50:23 -0700

    fixed camera location, less collision issues

--- Maze_Of_Doom/Intermediate/Build/Win64/UE4Editor/Development/Maze_Of_Doom/PCH.Maze_Of_Doom.h.cpp
+++ Maze_Of_Doom/Intermediate/Build/Win64/UE4Editor/Development/Maze_Of_Doom/PCH.Maze_Of_Doom.h.cpp
@@ -1,1 +1,1 @@
-#include "C:\Users\Owner\Documents\engine\Maze_Of_Doom\Source\Maze_Of_Doom\Maze_Of_Doom.h"
+#include "C:\Users\David\Desktop\ASU\SER432\Group2\Maze_Of_Doom\Source\Maze_Of_Doom\Maze_Of_Doom.h"
